ba2ea9cb5560f0124bdd38ce1c12d79fd537f2a6
Niraj81 <nirajpatidar81@gmail.com>
2023-12-22 18:56:27 +0530

    feat: Added functionality for creating group

--- android_client/app/src/main/java/com/niraj/alertly/network/AlertlyAPI.kt
+++ android_client/app/src/main/java/com/niraj/alertly/network/AlertlyAPI.kt
@@ -4,6 +4,8 @@ import com.niraj.alertly.BuildConfig
 import com.niraj.alertly.data.LoginRequest
 import com.niraj.alertly.data.LoginResponse
 import com.niraj.alertly.data.MyGroupResponse
+import com.niraj.alertly.data.creategroup.CreateGroupRequest
+import com.niraj.alertly.data.creategroup.CreateGroupResponse
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -19,6 +21,9 @@ interface AlertlyAPI {
 
     @GET("api/groups")
     suspend fun getGroups(@Header("Authorization") bearerToken :String): Response<MyGroupResponse>
+
+    @POST("api/groups/create")
+    suspend fun createGroup(@Header("Authorization") bearerToken :String, @Body createGroupRequest: CreateGroupRequest): Response<CreateGroupResponse>
 
     companion object {
         private var apiService: AlertlyAPI? = null
